72b2b713236726eb9719e27ca29267d944ea981d
santanusetu <santanu.setu@gmail.com>
2023-02-01 03:49:52 -0800

    commit

--- generate-graph.py
+++ generate-graph.py
@@ -3,8 +3,8 @@ from random import randint
 from datetime import datetime, timedelta
 
 # Set your desired start and end dates in yyyy-mm-dd format and the maximum number of commits per day
-start_date_str = "2022-09-01"
-end_date_str = "2022-09-07"
+start_date_str = "2023-02-01"
+end_date_str = "2023-05-23"
 max_commits_per_day = 4
 
 # Convert start and end date strings to datetime objects
